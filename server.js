a9177f9b5135a3e8f113f1c80f0973475e70645f
kumarvikas10 <vikasverma19960226@gmail.com>
2025-06-24 17:44:08 +0530

    Bad 502 Error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mediaRoutes = require('./routes/media');
+
 
 const app = express();
 app.use(cors());
@@ -14,15 +14,16 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB error:', err));
 
+const mediaRoutes = require('./routes/media');
+
 // Routes
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/cities', require('./routes/cities'));
+app.use('/api/media', mediaRoutes);
 
 app.get('/', (req, res) => {
   res.send('Sandy Photography API is running 🚀');
 });
-
-app.use('/api/media', mediaRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
